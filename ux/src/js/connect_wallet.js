0b89a4ea090f9072ac54f7011634330cf71bb523
nemaberci <bercike445@gmail.com>
2024-02-17 22:31:29 +0100

    Added wip contract upload

---
+++ ux/src/js/connect_wallet.js
@@ -0,0 +1,11 @@
+import * as util from '@polkadot/util';
+import * as utilCrypto from '@polkadot/util-crypto';
+import * as api from '@polkadot/api';
+import * as stuff from '@polkadot/extension-dapp';
+
+window.dapp_extension = {
+    ...stuff
+}
+window.polkadot_api = {
+    ...api
+}
